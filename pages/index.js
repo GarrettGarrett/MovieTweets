952624dfe340b51640aa114bc480f7852c73b59b
PyPy <skfelix1@gmail.com>
2022-02-01 00:07:03 -0800

    env variables

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ export default function Home() {
     }
     
   }
-
+// 
   async function asyncForEach(array, callback) {
     for (let index = 0; index < array?.length; index++) {
       await callback(array[index], index, array);
