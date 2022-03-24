111a8c172834d788f470c09727d32976b421e95f
PyPy <skfelix1@gmail.com>
2022-03-24 15:42:33 -0700

    updated api for get followers

--- pages/api/getFollowerCount.js
+++ pages/api/getFollowerCount.js
@@ -1,38 +1,26 @@
 
-
 export default async (req, res) => {
 
     if (req.method === 'POST') {
         let actor = req.body.actor
-        // console.log("🚀 ~ file: getFollowerCount.js ~ line 7 ~ actor", actor)
-
-        var myHeaders = new Headers();
-        myHeaders.append("Connection", "keep-alive");
-        myHeaders.append("sec-ch-ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"99\", \"Google Chrome\";v=\"99\"");
-        myHeaders.append("Accept", "*/*");
-        myHeaders.append("X-Requested-With", "XMLHttpRequest");
-        myHeaders.append("sec-ch-ua-mobile", "?0");
-        myHeaders.append("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36");
-        myHeaders.append("sec-ch-ua-platform", "\"macOS\"");
-        myHeaders.append("Sec-Fetch-Site", "same-origin");
-        myHeaders.append("Sec-Fetch-Mode", "cors");
-        myHeaders.append("Sec-Fetch-Dest", "empty");
-        myHeaders.append("Accept-Language", "en-US,en;q=0.9");
+        // console.log("🚀 ~ file: getFollowerCount.js ~ line 8 ~ actor", actor) //Daniel_CraigFan
         
-        var requestOptions = {
-          method: 'GET',
-          headers: myHeaders,
-          redirect: 'follow'
-        };
 
+
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+          };
+          
+          
+         
           
         // How to refresh this?
         //  - 
-        const response = await fetch(`https://mlmcounts.herokuapp.com/twitter/api/?name=${actor}`, requestOptions)
+        const response = await fetch(`https://cdn.syndication.twimg.com/widgets/followbutton/info.json?screen_names=${actor}`, requestOptions)
         // console.log("🚀 ~ file: getFollowerCount.js ~ line 15 ~ response", response)
         const response_json = await response.json()
-        // console.log("🚀 ~ file: getFollowerCount.js ~ line 37 ~ response_json", response_json)
-        let followers_count = response_json.followers_count
+        let followers_count = response_json[0].followers_count
         // console.log("🚀 ~ file: getFollowerCount.js ~ line 36 ~ followers_count", followers_count)
         
         if (response_json) {
